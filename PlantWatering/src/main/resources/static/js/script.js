4fa31368aa8516e9b0e64294699ad6f5bf4ce698
Diego Hoyos <hoyosdiegoa@gmail.com>
2020-04-18 20:53:22 -0600

    Able to display user info on homepage

--- PlantWatering/src/main/resources/static/js/script.js
+++ PlantWatering/src/main/resources/static/js/script.js
@@ -11,12 +11,12 @@ function init(){
 
 
 
-function getUserPlants(){
+function getUser(){
 	// TODO:
 	// * Use XMLHttpRequest to perform a GET request to "api/films/"
 	// with the filmId appended.
 	let xhr = new XMLHttpRequest();
-	xhr.open('GET', 'api/userplants/1');
+	xhr.open('GET', 'api/users/1');
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState === 4) {
 			switch (xhr.status) {
@@ -41,20 +41,20 @@ function getUserPlants(){
 
 }
 
-function displayUserPlants(userPlant) {
+function displayUser(user) {
 	let dataDiv = document.getElementById('eventDiv');
 	dataDiv.textContent = '';
 	// TODO:
 	// * Create and append elements to the data div to display:
 	// * Film title (h1) and description (blockquote).
-	console.log(userPlant);
+	console.log(user);
 	
-	// let h1 = document.createElement('h1');
-	// h1.textContent = 'Welcome ' + userPlant.;
-	// dataDiv.appendChild(h1);
-	// * Rating, release year, and length as an unordered list.
-	// let ul = document.createElement('ul');
-	// dataDiv.appendChild(ul);
+	let h1 = document.createElement('h1');
+	h1.textContent = 'Welcome ' + user.userName;
+	dataDiv.appendChild(h1);
+	let ul = document.createElement('ul');
+	dataDiv.appendChild(ul);
+	// var userPlants = user.
 	// let li = document.createElement('li');
 	// li.textContent = 'Rated ' + film.rating;
 	// ul.appendChild(li);
