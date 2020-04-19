5d11482fd9a1a477445c57aa90a376450596a735
Diego Hoyos <hoyosdiegoa@gmail.com>
2020-04-19 17:38:12 -0600

    finished userplant form

--- PlantWatering/src/main/resources/static/js/script.js
+++ PlantWatering/src/main/resources/static/js/script.js
@@ -60,14 +60,93 @@ function displayUserPlants(userPlants) {
 		userPlantButton.id = 'userPlantButton';
 		userPlantButton.className = 'btn btn-primary';
 		userPlantButton.textContent = 'Edit UserPlant';
-		userPlantButton.addEventListener('click', function(e){
-			console.log(e);
-			// showUserPlantForm(e.target);
-		});
 		ul.appendChild(userPlantButton);
-
+		userPlantButton.addEventListener('click', function(){
+			let userPlantFormDiv = document.createElement('div');
+			userPlantFormDiv.id = 'userPlantFormDiv'+element.id;
+			userPlantFormDiv.style.display = 'none';
+			ul.appendChild(userPlantFormDiv);
+			showUserPlantForm(element);
+		});
 		dataDiv.appendChild(ul);
 	});	
+}
+
+function showUserPlantForm(userPlant){
+	let formDiv = document.getElementById('userPlantFormDiv'+userPlant.id);
+	formDiv.textContent = '';
+
+	let form = document.createElement('form');
+	form.name = 'userPlantForm';
+
+	//GROUP FOR  NAME
+	let nameGroup = document.createElement('div');
+	nameGroup.className = 'form-group';
+
+	let nameLabel = document.createElement('label');
+	nameLabel.textContent = 'Plant Name';
+	nameGroup.appendChild(nameLabel);
+
+	let nameInput = document.createElement('input');
+	nameInput.placeholder = userPlant.name;
+	nameInput.name = 'name';
+	nameInput.type = 'text';
+	nameGroup.appendChild(nameInput);
+
+	form.appendChild(nameGroup);
+
+	//GROUP FOR LAST WATERING
+	let locationGroup = document.createElement('div');
+	locationGroup.className = 'form-group';
+
+	let locationLabel = document.createElement('label');
+	locationLabel.textContent = 'Location';
+	locationGroup.appendChild(locationLabel);
+
+	let locationInput = document.createElement('input');
+	locationInput.placeholder = userPlant.location;
+	locationInput.name = 'location';
+	locationInput.type = 'text';
+	locationGroup.appendChild(locationInput);
+
+	form.appendChild(locationGroup);
+
+	//GROUP FOR  WATERING
+	let wateringGroup = document.createElement('div');
+	wateringGroup.className = 'form-group';
+
+	let wateringLabel = document.createElement('label');
+	wateringLabel.textContent = 'To change watering times view userPlant details';
+	wateringGroup.appendChild(wateringLabel);
+
+	form.appendChild(wateringGroup);
+
+	//HIDDEN ID VALUE TO BUILD JSON OBJECT
+	let hiddenValue = document.createElement('input');
+	hiddenValue.type = 'hidden';
+	hiddenValue.name = 'id';
+	hiddenValue.value = userPlant.id;
+	form.appendChild(hiddenValue);
+
+	//BUTTON FOR SUBMIT
+	let editUserPlantButton = document.createElement('button');
+	editUserPlantButton.name = 'editUserPlantButton';
+	editUserPlantButton.id = 'editUserPlantButton';
+	editUserPlantButton.className = 'btn btn-primary';
+	editUserPlantButton.textContent = 'Submit Changes';
+	editUserPlantButton.addEventListener('click', function(user){
+		event.preventDefault();
+		// updateUserPlant(document.userPlantForm)
+	});
+	form.appendChild(editUserPlantButton);
+	//CONDITIONAL TO TOGGLE DISPLAY
+	if (formDiv.style.display === "none") {
+		formDiv.style.display = "block";
+
+	  } else {
+		formDiv.style.display = "none";
+	  }
+	formDiv.appendChild(form);			
 }
 
 //Function gets user and their plants... TODO to pass in id
@@ -117,6 +196,7 @@ function displayUser(user) {
 
 	let formDiv = document.createElement('div');
 	formDiv.id = 'formDiv';
+	formDiv.style.display = 'none';
 	dataDiv.appendChild(formDiv);
 }
 
